081f604a49ba0c4ac92efa8e3d79e8e216d3c828
Aurore-dw <aurore-corvest@hotmail.fr>
2020-07-02 10:01:34 +0200

    Changement du chemin de connexion db

--- model/Manager.php
+++ model/Manager.php
@@ -9,8 +9,7 @@ class Manager
 	{
 		try
 		{
-			//$db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', ''); //Localhost
-			$db = new PDO('mysql:host=db5000579977.hosting-data.io;dbname=dbs559572', 'dbu966292', 'GC8BO0FM#Es5');
+			$db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', ''); 
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 			return $db;
